712a662082cf3a0249b8075eecd0ce86183fd8f4
KovylyaevHSE <sasha_kovylyaev@mail.ru>
2025-04-29 09:39:43 +0300

    deleted trash

--- home/models.py
+++ home/models.py
@@ -8,21 +8,3 @@ class GrayscaleImage(models.Model):
 
     def __str__(self):
         return str(self.signature)
-    
-class ResizedImage(models.Model):
-    grayscale_image = models.OneToOneField(GrayscaleImage, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='resized_images/')
-
-    def __str__(self):
-        return f"Resized image for {self.grayscale_image.signature}"
-
-class AttentionedImage(models.Model):
-    grayscale_image = models.OneToOneField(GrayscaleImage, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='attentioned_images/')
-    download_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    predicted_class = models.TextField(default="I don’t know")
-    # temperature = ...
-    # composition = ...
-
-    def __str__(self):
-        return str(self.download_token)
